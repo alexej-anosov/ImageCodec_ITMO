0c5dd4cff18963b2ee6b0309e1c91e0772193c2c
Aleksey Anosov (MTS AI) <a.anosov@mts.ai>
2024-09-28 14:45:11 +0300

    inits

---
+++ src/modeling/base_ae_inits.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+
+from src.modeling.base import BaseModel
+
+
+class BaseAutoEncoder(BaseModel):
+    def __init__(self, init_method, model_name='base_auto_encoder'):
+        print(init_method)
+        super(BaseAutoEncoder, self).__init__(model_name)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=7, padding=3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.AvgPool2d(2),
+            nn.Conv2d(128, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.AvgPool2d(2),
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.AvgPool2d(2),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(
+                16, 32, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                32, 128, kernel_size=5, stride=2, padding=2, output_padding=1
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                128, 3, kernel_size=7, stride=2, padding=3, output_padding=1
+            ),
+            nn.Sigmoid(),
+        )
+
+        self.initialize_weights(init_method)
+
+    def initialize_weights(self, init_method):
+        def init_func(m):
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
+                if init_method == 'xavier':
+                    nn.init.xavier_uniform_(m.weight)
+                elif init_method == 'kaiming':
+                    nn.init.kaiming_uniform_(m.weight, nonlinearity='relu')
+                elif init_method == 'orthogonal':
+                    nn.init.orthogonal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+        
+        self.apply(init_func)
+
+    def forward(self, x, b_t=None):
+        x = self.encoder(x)
+        if self.training and b_t is not None:
+            max_val = x.max() / (2 ** (b_t + 1))
+            noise = torch.rand_like(x) * max_val
+            x = x + noise
+        x = self.decoder(x)
+        return x
