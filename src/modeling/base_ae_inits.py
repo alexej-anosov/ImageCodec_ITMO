30e8e5da5bc10dcaa09b31558703179409863c59
Aleksey Anosov (MTS AI) <a.anosov@mts.ai>
2024-09-28 16:57:05 +0300

    bugfix

--- src/modeling/base_ae_inits.py
+++ src/modeling/base_ae_inits.py
@@ -5,7 +5,7 @@ from src.modeling.base import BaseModel
 
 
 class BaseAutoEncoderWithInits(BaseModel):
-    def __init__(self, init_method, model_name='base_auto_encoder'):
+    def __init__(self, model_name='base_auto_encoder', init_method=None):
         super(BaseAutoEncoder, self).__init__(model_name)
         print(init_method)
         self.encoder = nn.Sequential(
