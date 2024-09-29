e6e36cd54e42eda1d6646bf63f58b095ec7dffe6
Aleksey Anosov (MTS AI) <a.anosov@mts.ai>
2024-09-29 23:38:14 +0300

    bottleneck_v4

--- src/modeling/base_ae_inits_bottleneck.py
+++ src/modeling/base_ae_inits_bottleneck.py
@@ -4,35 +4,41 @@ import torch.nn as nn
 from src.modeling.base import BaseModel
 
 
-class BaseAutoEncoderWithInitsBottleneck(BaseModel):
+class BaseAutoEncoderWithInits(BaseModel):
     def __init__(self, model_name='base_auto_encoder', init_method=None):
-        super(BaseAutoEncoderWithInitsBottleneck, self).__init__(model_name)
+        super(BaseAutoEncoderWithInits, self).__init__(model_name)
+        print(init_method)
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, padding=3),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(3, 128, kernel_size=7, padding=3),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.AvgPool2d(2),
-            nn.Conv2d(64, 16, kernel_size=3, padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(128, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.AvgPool2d(2),
-            nn.Conv2d(16, 12, kernel_size=3, padding=1),
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),
             nn.BatchNorm2d(12),
             nn.ReLU(),
             nn.AvgPool2d(2),
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(12, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(
+                12, 32, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(
+                32, 128, kernel_size=5, stride=2, padding=2, output_padding=1
+            ),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 3, kernel_size=7, stride=2, padding=3, output_padding=1),
+            nn.ConvTranspose2d(
+                128, 3, kernel_size=7, stride=2, padding=3, output_padding=1
+            ),
             nn.Sigmoid(),
         )
 
         self.initialize_weights(init_method)
-
 
     def initialize_weights(self, init_method):
         def init_func(m):
@@ -48,7 +54,6 @@ class BaseAutoEncoderWithInitsBottleneck(BaseModel):
         
         self.apply(init_func)
 
-        
     def forward(self, x, b_t=None):
         x = self.encoder(x)
         if self.training and b_t is not None:
